8c3dc43f091925dcddc946b1458662b55ed8b202
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-12-11 20:24:12 +0100

    Add solution

--- src/2.ts
+++ src/2.ts
@@ -16,9 +16,9 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -32,6 +32,10 @@ class Employee {
 }
 
 class Manager extends Employee {
+  constructor() {
+    super("Anny", "Main", 1000);
+    this.salary += 10000;
+  }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
 
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,39 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number = Math.random()) { }
+  
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-const house = new MyHouse(key);
-const person = new Person(key);
+class Person {
+  constructor(private key: Key) {}
+  
+  getKey():Key {
+    return this.key;
+  }
+}
 
-house.openDoor(person.getKey());
+abstract class House {
+  constructor(public door: boolean, public key: Key) { }
+  
+  comeIn() {
+    if(this.door) {}
+  }
+}
 
-house.comeIn(person);
+class MyHouse extends House {
+  openDoor() {};
+}
+
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
 
 
 export {};
